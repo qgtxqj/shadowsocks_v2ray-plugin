a4750d83d58271fb5a0e0f7157ed75780d660d31
中男顽童 <qgtxqj@gmail.com>
2019-07-24 10:52:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,7 +25,7 @@ tar xvf demo.tar.gz
 rm -rf demo.tar.gz
 
 cd /tmp
-wget -qO v2ray-plugin-linux-amd64-v1.1.0.tar.gz https://41-164596259-gh.circle-artifacts.com/0/bin/v2ray-plugin-linux-amd64-v1.1.0-8-g465426e.tar.gz
+wget -qO v2ray-plugin-linux-amd64-v1.1.0.tar.gz https://42-164596259-gh.circle-artifacts.com/0/bin/v2ray-plugin-linux-amd64-v1.1.0-9-g2e56b2b.tar.gz
 tar zxf v2ray-plugin-linux-amd64-v1.1.0.tar.gz
 cp v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
 rm -f v2ray-plugin*
