d39113ed93fc17cfcb146f33eba64ee4b3ce0e76
unknown <qgtxqj@gmail.com>
2019-10-03 11:41:20 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -3,9 +3,22 @@ FROM debian:stretch-backports
 RUN set -ex \
     && sh -c 'printf "deb http://deb.debian.org/debian stretch-backports-sloppy main" > /etc/apt/sources.list.d/stretch-backports.list' \
 		&& apt-get update \
-		&& apt-get install -y --no-install-recommends wget tar ca-certificates \
+		&& apt-get install -y --no-install-recommends wget tar ca-certificates curl \
 		&& apt-get -t stretch-backports-sloppy install -y --no-install-recommends shadowsocks-libev \
-		&& rm -rf /var/lib/apt/lists/*
+		&& rm -rf /var/lib/apt/lists/* \
+		&& curl https://getcaddy.com | bash -s personal tls.dns.cloudflare \
+		&& mkdir /wwwroot \
+    && cd /wwwroot \
+    && wget --no-check-certificate -qO 'demo.tar.gz' "https://github.com/xianren78/v2ray-heroku/raw/master/demo.tar.gz" \
+    && tar xvf demo.tar.gz \
+    && rm -rf demo.tar.gz \
+    && cd /tmp \
+    && wget -qO v2ray-plugin-linux-amd64-v1.1.0.tar.gz https://52-164596259-gh.circle-artifacts.com/0/bin/v2ray-plugin-linux-amd64-v1.1.0-20-gca36119.tar.gz \
+    && tar zxf v2ray-plugin-linux-amd64-v1.1.0.tar.gz \
+    && cp v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin \
+    && rm -f v2ray-plugin* \
+    && rm -rf /etc/localtime \
+    && ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 
 ADD entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,36 +1,6 @@
 #! /bin/bash
 
-rm -rf /etc/localtime
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-date -R
-
-SYS_Bit="$(getconf LONG_BIT)"
-[[ "$SYS_Bit" == '32' ]] && BitVer='_linux_386.tar.gz'
-[[ "$SYS_Bit" == '64' ]] && BitVer='_linux_amd64.tar.gz'
-
-
-C_VER=`wget -qO- "https://api.github.com/repos/mholt/caddy/releases/latest" | grep 'tag_name' | cut -d\" -f4`
-mkdir /caddybin
-cd /caddybin
-wget --no-check-certificate -qO 'caddy.tar.gz' "https://github.com/mholt/caddy/releases/download/$C_VER/caddy_$C_VER$BitVer"
-tar xvf caddy.tar.gz
-rm -rf caddy.tar.gz
-chmod +x caddy
-cd /root
-mkdir /wwwroot
-cd /wwwroot
-
-wget --no-check-certificate -qO 'demo.tar.gz' "https://github.com/xianren78/v2ray-heroku/raw/master/demo.tar.gz"
-tar xvf demo.tar.gz
-rm -rf demo.tar.gz
-
-cd /tmp
-wget -qO v2ray-plugin-linux-amd64-v1.1.0.tar.gz https://52-164596259-gh.circle-artifacts.com/0/bin/v2ray-plugin-linux-amd64-v1.1.0-20-gca36119.tar.gz
-tar zxf v2ray-plugin-linux-amd64-v1.1.0.tar.gz
-cp v2ray-plugin_linux_amd64 /usr/bin/v2ray-plugin
-rm -f v2ray-plugin*
-
-cat <<-EOF > /caddybin/Caddyfile
+cat <<-EOF > /etc/Caddyfile
 http://0.0.0.0:${PORT}
 {
 	root /wwwroot
@@ -43,8 +13,6 @@ http://0.0.0.0:${PORT}
 }
 EOF
 
-
-cd /caddybin
-./caddy -conf="Caddyfile" &
+/usr/local/bin/caddy -conf=/etc/Caddyfile &
 
 ss-server -s 127.0.0.1 -p 10001 -k $PASSWORD -m $METHOD -t 300 -d $DNS --plugin v2ray-plugin --plugin-opts "server;path=${SS_Path}" $ARGS
