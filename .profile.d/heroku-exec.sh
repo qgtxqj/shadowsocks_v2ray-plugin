56a72cf450c8e2a6fb3b5788d1e1b77af0e2e161
unknown <qgtxqj@gmail.com>
2020-07-23 21:03:27 +0800

    Update heroku-exec.sh

--- .profile.d/heroku-exec.sh
+++ .profile.d/heroku-exec.sh
@@ -1,4 +1,4 @@
-[ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
+﻿[ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL")
 
   if [ $(ps -ef | grep "ss-server" | grep -v "grep" | wc -l) -eq 0 ]
   then
